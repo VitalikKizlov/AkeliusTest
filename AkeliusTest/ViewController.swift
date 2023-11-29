8f49dcbbd2d030e8603a0d21a902fe69677d276e
Vitalii Kizlov <vitalii kizlov>
2023-11-29 17:01:26 +0100

    Initial commit

--- AkeliusTest/ViewController.swift
+++ AkeliusTest/ViewController.swift
@@ -6,14 +6,66 @@
 //
 
 import UIKit
+import WebKit
 
 class ViewController: UIViewController {
 
+    private var webView: WKWebView!
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        setupWebView()
     }
 
+    private func setupWebView() {
+        let configuration = WKWebViewConfiguration()
+        configuration.userContentController.add(self, name: "buttonClickHandler")
+        configuration.userContentController.add(self, name: "onStart")
 
+        let js = """
+            window.webkit.messageHandlers.onStart.postMessage("onStart");
+            """
+
+        let onStartScript = WKUserScript(source: js, injectionTime: .atDocumentEnd, forMainFrameOnly: false)
+        configuration.userContentController.addUserScript(onStartScript)
+
+        webView = WKWebView(frame: view.bounds, configuration: configuration)
+        view.addSubview(webView)
+        
+        webView.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            webView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            webView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            webView.bottomAnchor.constraint(equalTo: view.bottomAnchor),
+            webView.topAnchor.constraint(equalTo: view.topAnchor)
+        ])
+
+        if let url = Bundle.main.url(forResource: "index", withExtension: "html") {
+            webView.loadFileURL(url, allowingReadAccessTo: url.deletingLastPathComponent())
+        }
+    }
+}
+
+// MARK: - WKScriptMessageHandler
+
+extension ViewController: WKScriptMessageHandler {
+    func userContentController(_ userContentController: WKUserContentController, didReceive message: WKScriptMessage) {
+        switch message.name {
+        case "onStart":
+            let script = "document.getElementById('the_other_image').src = './cat.jpeg';"
+
+            webView.evaluateJavaScript(script) { _, error in
+                if let error = error {
+                    print("Error evaluating JavaScript: \(error)")
+                } else {
+                    print("JavaScript executed successfully")
+                }
+            }
+        case "buttonClickHandler":
+            print("button clicked")
+        default:
+            break
+        }
+    }
 }
 
